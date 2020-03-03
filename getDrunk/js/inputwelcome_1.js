57bc329f2677236957fe000fc80fc6358055508b
Lucas Schneider <lucas.schneider@mail.de>
2020-03-03 12:53:09 +0100

    Fixed Popup functionality

---
+++ getDrunk/js/inputwelcome_1.js
@@ -0,0 +1,8 @@
+
+document.getElementById('btn_1').addEventListener('click',
+  function() {
+        console.log(1);
+        let modal = document.getElementById('popup_1');
+        modal.style.display='block';
+  }
+);
