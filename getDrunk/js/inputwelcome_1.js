4838d08b083763872090cc66dc543def16b749c0
yiwjia <48102647+yiwjia@users.noreply.github.com>
2020-03-05 12:45:20 +0100

    Add files via upload
    new vertion of popup

--- getDrunk/js/inputwelcome_1.js
+++ getDrunk/js/inputwelcome_1.js
@@ -1,8 +1,60 @@
 
-document.getElementById('btn_1').addEventListener('click',
+var modal={
+  popup: document.getElementsByClassName('popup'),
+  main : document.getElementById('are'),
+  foot : document.getElementById('side_foot'),
+  btn:document.getElementsByClassName('btn'),
+  close:document.getElementsByClassName('close'),
+}
+
+modal.btn[0].addEventListener('click',
   function() {
-        console.log(1);
-        let modal = document.getElementById('popup_1');
-        modal.style.display='block';
+        modal.popup[0].style.display='block';
+        modal.main.style.display='none';
+        modal.foot.style.display='none';
+  }
+
+);
+
+modal.close[0].addEventListener('click',
+  function() {
+        modal.foot.style.display='block';
+        modal.popup[0].style.display='none';
+        modal.main.style.display='block';
+  }
+);
+
+
+modal.btn[1].addEventListener('click',
+  function() {
+        modal.popup[1].style.display='block';
+        modal.main.style.display='none';
+        modal.foot.style.display='none';
+  }
+
+);
+
+modal.close[1].addEventListener('click',
+  function() {
+        modal.foot.style.display='block';
+        modal.popup[1].style.display='none';
+        modal.main.style.display='block';
+  }
+);
+
+modal.btn[2].addEventListener('click',
+  function() {
+        modal.popup[2].style.display='block';
+        modal.main.style.display='none';
+        modal.foot.style.display='none';
+  }
+
+);
+
+modal.close[2].addEventListener('click',
+  function() {
+        modal.foot.style.display='block';
+        modal.popup[2].style.display='none';
+        modal.main.style.display='block';
   }
 );
