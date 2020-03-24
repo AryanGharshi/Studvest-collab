98f17a2c71801a727ec353302db0c590ac363a02
yiwjia <48102647+yiwjia@users.noreply.github.com>
2020-03-24 16:26:36 +0100

    Add files via upload

---
+++ getDrunk/js/input_reg.js
@@ -0,0 +1,24 @@
+
+function reg(n){
+  var item=String('barinput'+n);
+  item=document.getElementById(item);
+  item.removeAttribute('disabled');
+}
+
+function reg_2(n){
+  var item=String('drinksinput'+n);
+  item=document.getElementById(item);
+  item.removeAttribute('disabled');
+}
+
+function reg_3(n){
+  var item=String('tagsinput'+n);
+  item=document.getElementById(item);
+  item.removeAttribute('disabled');
+}
+
+function reg_4(n){
+  var item=String('menusinput'+n);
+  item=document.getElementById(item);
+  item.removeAttribute('disabled');
+}
