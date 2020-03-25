54191dfafdec2d4e707c94a5baee5443d8e34ed9
yiwjia <48102647+yiwjia@users.noreply.github.com>
2020-03-25 17:15:51 +0100

    Add files via upload

--- getDrunk/js/input_del.js
+++ getDrunk/js/input_del.js
@@ -1,29 +1,12 @@
-/*var remove={
-  del_one: document.getElementsByClassName('delete'),
-  item: document.getElementsByClassName('item'),
-}
-remove.del_one[0].addEventListener('click',
-  function() {
-        remove.item[0].style.display='none';
-  }
-);
-*/
-function del($n){
-  var item=String('barname'+$n);
+
+function del(section,id){
+  var item=String(section+id);
   document.getElementById(item).style.display='none';
 }
 
-function del2($n){
-  var item=String('drinks'+$n);
-  document.getElementById(item).style.display='none';
-}
 
-function del3($n){
-  var item=String('tags'+$n);
-  document.getElementById(item).style.display='none';
-}
-
-function del4($n){
-  var item=String('menus'+$n);
-  document.getElementById(item).style.display='none';
+function reg(section,id){
+  var item=String(section+id);
+  item=document.getElementById(item);
+  item.removeAttribute('disabled');
 }
