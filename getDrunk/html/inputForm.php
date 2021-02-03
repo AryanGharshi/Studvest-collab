c1604889f1e3a14d5d5d56b016285eccab51dcd0
KornelijaK <46718930+KornelijaK@users.noreply.github.com>
2021-02-03 18:13:50 +0100

    menu

--- getDrunk/html/inputForm.php
+++ getDrunk/html/inputForm.php
@@ -40,6 +40,7 @@ function console_log( $data ){
 <body id="body-main">
 
 <!-- SQL COMMANDS -->
+
 <?php
     # Delete Drinks / Drink Types / Tags / Bars
     if(isset($_POST['del'])){
@@ -168,6 +169,17 @@ function console_log( $data ){
         <?php include('header.php'); ?>
     </a>
 
+    <!-- Info page -->
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+  <span onclick="openNav()">(OPEN)</span>
     <div id="main" class="main">
         <div class="above-list">
             <h1>Welcome back.</h1>
--- getDrunk/js/inputForm.js
+++ getDrunk/js/inputForm.js
@@ -90,3 +90,14 @@ function close_popup(source, target) {
     document.getElementById(target).style.filter ='none';
 }
 
+
+// INFORMATION //
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
